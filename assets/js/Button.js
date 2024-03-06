431eb806784a99d950b53862b167032b2f73cb34
Gary Matthews <18332743+GaryMatthews@users.noreply.github.com>
2024-03-06 22:50:58 +1000

    Publish commit

--- assets/js/Button.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "./core/jsx-runtime";
-const Button = ({
-  children,
-  onClick
-}) => _jsx("button", {
-  onClick: onClick,
-  children: children
-});
-export default Button;
